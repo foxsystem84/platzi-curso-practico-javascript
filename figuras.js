92f564ffc3a74c6f3b3ac88b13d68f564d4d9637
Grover84 <gllanosh@gmail.com>
2021-07-04 00:16:14 -0500

    Final Taller 2

--- figuras.js
+++ figuras.js
@@ -36,3 +36,52 @@ function calculaAreaCuadrado(){
     document.getElementById("resultado").innerHTML = "Area mide: "+area;
 
 }
+
+// TALLER 2
+const cupons = [
+    {
+        nombre: "platzi",
+        descuento: 10
+    },
+    {
+        nombre: "estudiante",
+        descuento: 15
+    },
+    {
+        nombre: "empresa",
+        descuento: 20, 
+    }
+];
+
+const inputPrecio = document.getElementById("inputPrecio").value;
+const inputDescuento = document.getElementById("inputDescuento").value;
+const inputCupon = document.getElementById("inputCupon").value;
+
+function precioDescuento(precio,descuento){
+    const total = precio - (precio*descuento)/100;   
+    return total;   
+}
+
+function precioDescuentoCupon(cupon){
+    const cuponDescuento = cupons.find(c =>c.nombre === cupon);    
+    if(cuponDescuento){
+        const descuentoCupon = cuponDescuento.descuento;    
+        return descuentoCupon;
+    }
+    /*else{
+        console.log("descuento no disponible");   }*/
+}
+
+function calcularPrecio(){
+    const precioTotalDescuento = precioDescuento(inputPrecio, inputDescuento); 
+    const descuentoCupon = precioDescuentoCupon(inputCupon);
+    const sumaDescuento= parseInt(inputDescuento) + parseInt(descuentoCupon);
+    const precioTotalDescuentoBono = precioDescuento(inputPrecio,sumaDescuento);
+
+    if(inputCupon == ''){
+        console.log(precioTotalDescuento);
+    }else{
+        console.log(precioTotalDescuentoBono);
+    }
+    
+}
